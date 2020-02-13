0de2fb0b48473466abfb0ad3a5e2732ceaccf8a8
Sheik Arbaz <sheik.arbaz@thoughtworks.com>
2020-02-13 22:12:10 +0530

    Characteristic test added

--- src/test/java/com/movierental/CustomerTest.java
+++ src/test/java/com/movierental/CustomerTest.java
@@ -2,8 +2,27 @@ package com.movierental;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class CustomerTest {
+
     @Test
-    public void test(){}
+    public void shouldReturnStatementForTheNewReleaseMovieRented() {
+        Customer customer = new Customer("Vivek");
+        Rental rental = warMovieFOrRental();
+        customer.addRental(rental);
+        String expectedResult = "Rental Record for Vivek\n" +
+                "\tWar\t15.0\n" +
+                "Amount owed is 15.0\n" +
+                "You earned 2 frequent renter points";
+
+        assertEquals(expectedResult, customer.statement());
+    }
+
+    private Rental warMovieFOrRental() {
+        Movie warMovie = new Movie("War", Movie.NEW_RELEASE);
+        return new Rental(warMovie, 5);
+    }
+
 
 }
